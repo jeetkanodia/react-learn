ce380be0fd1e73330e6b7a1c8d76fa7d4f516851
jeetkanodia <jeetkanodia@gmail.com>
2023-08-29 17:01:42 +0530

    ft: sidebar

---
+++ two/src/App.js
@@ -0,0 +1,16 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
