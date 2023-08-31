0af7af4ff7c8f3ee56a1bba7754699eb12329290
jeetkanodia <jeetkanodia@gmail.com>
2023-08-31 21:18:26 +0530

    add exchange and coin page

--- three/src/components/CoinDetails.jsx
+++ three/src/components/CoinDetails.jsx
@@ -1,9 +1,133 @@
-import React from 'react'
+import {
+  Badge,
+  Box,
+  Container,
+  HStack,
+  Image,
+  Progress,
+  Radio,
+  RadioGroup,
+  Stat,
+  StatArrow,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import Loader from "./Loader";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { server } from "../index";
+import { useParams } from "react-router-dom";
+import Error from "./Error";
 
 const CoinDetails = () => {
-  return (
-    <div>CoinDetails</div>
-  )
-}
+  const [coin, setCoin] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currency, setCurrency] = useState("inr");
 
-export default CoinDetails
+  const params = useParams();
+
+  const changeCurrency = (currency) => {
+    setLoading(true);
+    setCurrency(currency);
+  };
+
+  const currencySymbol =
+    currency === "inr" ? "₹" : currency === "eur" ? "€" : "$";
+
+  useEffect(() => {
+    const fetchCoin = async () => {
+      try {
+        const { data } = await axios.get(`${server}/coins/${params.id}`);
+        setCoin(data);
+        console.log(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchCoin();
+  }, [params.id, currency]);
+
+  if (error) return <Error message={"Error while fetching the coin"} />;
+
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Box width={"full"} borderWidth={1}></Box>
+
+          <RadioGroup value={currency} onChange={changeCurrency} p={8}>
+            <HStack spacing={"4"}>
+              <Radio value={"inr"}>INR (₹)</Radio>
+              <Radio value={"eur"}>EURO (€)</Radio>
+              <Radio value={"usd"}>USD ($)</Radio>
+            </HStack>
+          </RadioGroup>
+
+          <VStack spacing={"4"} p={"16"} alignItems={"flex-start"}>
+            <Text alignSelf={"center"} opacity={"0.7"} fontSize={"small"}>
+              Last Updated on{" "}
+              {Date(coin.market_data.last_updated).split("G")[0]}
+            </Text>
+            <Image
+              src={coin.image.large}
+              w={"16"}
+              h={"16"}
+              objectFit={"contain"}
+            />
+
+            <Stat>
+              <StatLabel>{coin.name}</StatLabel>
+              <StatNumber>
+                {currencySymbol}
+                {coin.market_data.current_price[currency]}
+              </StatNumber>
+              <StatHelpText>
+                <StatArrow
+                  type={
+                    coin.market_data.price_change_percentage_24h > 0
+                      ? "increase"
+                      : "decrease"
+                  }
+                />
+                {coin.market_data.price_change_percentage_24h}%
+              </StatHelpText>
+            </Stat>
+            <Badge fontSize={"2xl"} color={"white"} bgColor={"blackAlpha.800"}>
+              {`#${coin.market_cap_rank}`}
+            </Badge>
+
+            <CustomBar
+              high={`${currencySymbol}${coin.market_data.high_24h[currency]}`}
+              low={`${currencySymbol}${coin.market_data.low_24h[currency]}`}
+            />
+
+            <Box></Box>
+          </VStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+const CustomBar = ({ high, low }) => {
+  return (
+    <VStack w={"full"}>
+      <Progress value={50} colorScheme={"teal"} w={"full"} />
+      <HStack justifyContent={"space-between"} w={"full"}>
+        <Badge children={low} colorScheme={"red"} />
+        <Text fontSize={"sm"}>24H Range</Text>
+        <Badge children={high} colorScheme={"green"} />
+      </HStack>
+    </VStack>
+  );
+};
+export default CoinDetails;
--- three/src/components/Exchanges.jsx
+++ three/src/components/Exchanges.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { server } from "../index";
+import { Container, HStack } from "@chakra-ui/react";
+import Loader from "./Loader";
+import ExchangeCard from "./ExchangeCard";
+import Error from "./Error";
 
 const Exchanges = () => {
-  return (
-    <div>Exchanges</div>
-  )
-}
+  const [exchanges, setExchanges] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fetchExchanges = async () => {
+      try {
+        const { data } = await axios.get(`${server}/exchanges`);
+        setExchanges(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchExchanges();
+  }, []);
 
-export default Exchanges
+
+  if(error) return <Error message={'Error while fetching coins'}/>
+  
+  return (
+    <Container maxW={"container.xl"}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <HStack wrap={"wrap"} justifyContent={'space-evenly'}>
+            {exchanges.map((i) => (
+              <ExchangeCard
+                key={i.id}
+                name={i.name}
+                url={i.url}
+                img={i.image}
+                rank={i.trust_score_rank}
+              />
+            ))}
+          </HStack>
+        </>
+      )}
+    </Container>
+  );
+};
+
+export default Exchanges;
