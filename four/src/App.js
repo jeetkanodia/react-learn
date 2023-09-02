5364f5ea993531472757a686c7670b3e9dcf870c
jeetkanodia <jeetkanodia@gmail.com>
2023-09-02 17:14:01 +0530

    inital project four

---
+++ four/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import Header from './components/Header'
+import "./styles/app.scss";
+const App = () => {
+  return <>
+  <Router>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+  </Router>
+  </>;
+};
+
+export default App;
